e06eb9250a1c30e3b2c1863a6e831348ba1f783d
SAbelle <sabelle@alumno.huergo.edu.ar>
2022-05-02 11:20:33 -0300

    Almost finished

--- regexpractice1.py
+++ regexpractice1.py
@@ -1,4 +1,3 @@
-from ast import Continue
 import re
 import os
 import msvcrt as m
@@ -16,6 +15,8 @@ inputresult : bool
 inputresult = True
 usersdata : list
 usersdata = []
+usersmatches : list
+usersmatches = []
 namehelper : str
 namehelper = ""
 surnamehelper : str
@@ -74,7 +75,6 @@ def data_input():
                 result = data_check(userinput,str)
                 if(result):
                     result = mail_check(userinput)
-                    print(result)
                     if(result):
                         mailhelper = userinput.lower()
                         error = False
@@ -97,8 +97,144 @@ def mail_check(data):
         return False
 
 #Data search function
-def user_search():
-    pass
+def user_search(data,field):
+    if(field == 1):
+        i = 0
+        for i in range (0, len(usersdata)):
+            if(re.findall("^" + data,usersdata[i][0])):
+                if(len(usersmatches) != 0):
+                    usersmatches.clear()
+                usersmatches.append(usersdata[i])
+                return True
+            else:
+                return False
+    elif(field == 2):
+        i = 0
+        for i in range (0, len(usersdata)):
+            if(re.findall(data + "$",usersdata[i][1])):
+                if(len(usersmatches) != 0):
+                    usersmatches.clear()
+                usersmatches.append(usersdata[i])
+                return True
+            else:
+                return False
+    elif(field == 3):
+        i = 0
+        for i in range (0, len(usersdata)):
+            if(re.findall(data,usersdata[i][2])):
+                if(len(usersmatches) != 0):
+                    usersmatches.clear()
+                usersmatches.append(usersdata[i])
+                return True
+            else:
+                return False
+    elif(field == 4):
+        i = 0
+        for i in range (0, len(usersdata)):
+            if(re.findall("^" + data + "$",usersdata[i][3])):
+                if(len(usersmatches) != 0):
+                    usersmatches.clear()
+                usersmatches.append(usersdata[i])
+                return True
+            else:
+                return False
+
+def console_text_search():
+    print("What do you want to search for?")
+    print("Options:")
+    print("1-User Name / 2-User Surname / 3-User Age / 4-User Mail")
+    action = int(input("Introduce the option number: "))
+    #Name option
+    if(action == 1):
+        userinput = input("Introduce the users name: ")
+        result = data_check(userinput,str)
+        if(result):
+            namehelper = userinput.lower()
+            result = user_search(namehelper,1)
+            if(result):
+                print("---MATCHES FOUND---")
+                print(usersmatches)
+            else:
+                print("---NOT MATCHES FOUND---")
+        else:
+            #Error
+            print("---ERROR---")
+            print("Try again, please")
+        print("Press Enter to continue...")
+        m.getch()
+        if(len(usersmatches) != 0):
+            usersmatches.clear()
+        os.system('cls')
+    #Surname option
+    elif(action == 2):
+        userinput = input("Introduce the users surname: ")
+        result = data_check(userinput,str)
+        if(result):
+            surnamehelper = userinput.lower()
+            result = user_search(surnamehelper,2)
+            if(result):
+                print("---MATCHES FOUND---")
+                print(usersmatches)
+            else:
+                print("---NOT MATCHES FOUND---")
+        else:
+            #Error
+            print("---ERROR---")
+            print("Try again, please")
+        print("Press Enter to continue...")
+        m.getch()
+        if(len(usersmatches) != 0):
+            usersmatches.clear()
+        os.system('cls')
+    #Age option
+    elif(action == 3):
+        numuserinput = input("Introduce the users age: ")
+        result = data_check(numuserinput,int)
+        if(result):
+            agehelper = numuserinput
+            result = user_search(agehelper,3)
+            if(result):
+                print("---MATCHES FOUND---")
+                print(usersmatches)
+            else:
+                print("---NOT MATCHES FOUND---")
+        else:
+            #Error
+            print("---ERROR---")
+            print("Try again, please")
+        print("Press Enter to continue...")
+        m.getch()
+        if(len(usersmatches) != 0):
+            usersmatches.clear()
+        os.system('cls')
+    #Mail option
+    elif(action == 4):
+        userinput = input("Introduce the users mail: ")
+        result = data_check(userinput,str)
+        if(result):
+            namehelper = userinput.lower()
+            result = user_search(namehelper,4)
+            if(result):
+                print("---MATCHES FOUND---")
+                print(usersmatches)
+            else:
+                print("---NOT MATCHES FOUND---")
+        else:
+            #Error
+            print("---ERROR---")
+            print("Try again, please")
+        print("Press Enter to continue...")
+        m.getch()
+        if(len(usersmatches) != 0):
+            usersmatches.clear()
+        os.system('cls')
+    #Error option
+    else:
+        print("---ERROR---")
+        print("The entered data is incorrect, please enter it again")
+        print("Press Enter to continue...")
+        m.getch()
+        os.system('cls')
 
 #Main while
 while True:
@@ -127,13 +263,7 @@ while True:
     elif(action == 2):
         os.system('cls')
         print("---USER SEARCH---")
-        print(usersdata)
-        #Requests the that wants to be found
-        #Calls user_search(parameters)
-        #Prints results
-        print("Press Enter to continue...")
-        m.getch()
-        os.system('cls')
+        console_text_search()
     #Exit option
     elif(action == 3):
         os.system('cls')
